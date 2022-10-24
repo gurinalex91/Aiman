c5a9023aac919f88b03c13f154f34df4d58cd890
Gurin Alexandr <gurinalex91@gmail.com>
2022-10-24 19:56:41 +0300

    add animation to page

--- js/script.js
+++ js/script.js
@@ -1,57 +1,70 @@
 window.addEventListener("DOMContentLoaded", () => {
-    const tabs = document.querySelectorAll(".tab-head__item"),
-      tabsContent = document.querySelectorAll(".tab-body__container"),
-      tabsParent = document.querySelector(".tab-head__container");
-  
-    function hideTabContent() {
-      tabsContent.forEach((i) => {
-        i.style.display = "none";
-      });
-      tabs.forEach((i) => {
-        i.classList.remove("active");
+  const tabs = document.querySelectorAll(".tab-head__item"),
+    tabsContent = document.querySelectorAll(".tab-body__container"),
+    tabsParent = document.querySelector(".tab-head__container");
+
+  function hideTabContent() {
+    tabsContent.forEach((i) => {
+      i.style.display = "none";
+    });
+    tabs.forEach((i) => {
+      i.classList.remove("active");
+    });
+  }
+  hideTabContent();
+  function showTabContent(i = 0) {
+    tabsContent[i].style.display = "block";
+    tabs[i].classList.add("active");
+  }
+  showTabContent();
+
+  tabsParent.addEventListener("click", (e) => {
+    if (e.target.classList.contains("tab-head__item")) {
+      tabs.forEach((item, i) => {
+        if (e.target == item) {
+          hideTabContent();
+          showTabContent(i);
+        }
       });
     }
-    hideTabContent();
-    function showTabContent(i = 0) {
-      tabsContent[i].style.display = "block";
-      tabs[i].classList.add("active");
-    }
-    showTabContent();
-  
-    tabsParent.addEventListener("click", (e) => {
-      if (e.target.classList.contains("tab-head__item")) {
-        tabs.forEach((item, i) => {
-          if (e.target == item) {
-            hideTabContent();
-            showTabContent(i);
-          }
-        });
+  });
+
+  //Swiper
+  const swiperSupport = new Swiper(".swiper-support", {
+    slidesPerView: 5.5,
+    speed: 400,
+    spaceBetween: 0,
+    grabCursor: true,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+  const swiperReviews = new Swiper(".swiper-reviews", {
+    slidesPerView: 1,
+    speed: 400,
+    spaceBetween: 0,
+    grabCursor: true,
+    autoHeight: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  //animation
+  function onEntry(e) {
+    e.forEach((change) => {
+      if (change.isIntersecting) {
+        change.target.classList.add("element-show");
       }
     });
-
-    //Swiper
-    const swiperSupport = new Swiper('.swiper-support', {
-      slidesPerView: 5.5,
-      speed: 400,
-      spaceBetween: 0,
-      grabCursor: true,
-      loop: true,
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-    });
-    const swiperReviews = new Swiper('.swiper-reviews', {
-      slidesPerView: 1,
-      speed: 400,
-      spaceBetween: 0,
-      grabCursor: true,
-      autoHeight: true,
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-    });
-    
-    
-  });
+  }
+  const options = { threshold: [0.5] };
+  const observer = new IntersectionObserver(onEntry, options);
+  const elements = document.querySelectorAll(".element-animation");
+  for (const elm of elements) {
+    observer.observe(elm);
+  }
+});
