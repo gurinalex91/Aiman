7a422083afa0c8340d01e8ce2f6ac05ee14b9041
Gurin Alexandr <gurinalex91@gmail.com>
2022-10-27 17:48:50 +0300

    add burger menu

--- js/script.js
+++ js/script.js
@@ -67,4 +67,43 @@ window.addEventListener("DOMContentLoaded", () => {
   for (const elm of elements) {
     observer.observe(elm);
   }
+
+  // Burger menu
+  // open
+  const burger = document.querySelectorAll(".navbar-burger");
+  const menu = document.querySelectorAll(".navbar-menu");
+
+  if (burger.length && menu.length) {
+    for (let i = 0; i < burger.length; i++) {
+      burger[i].addEventListener("click", function () {
+        for (let j = 0; j < menu.length; j++) {
+          menu[j].classList.toggle("hidden");
+        }
+      });
+    }
+  }
+
+  // close
+  const close = document.querySelectorAll(".navbar-close");
+  const backdrop = document.querySelectorAll(".navbar-backdrop");
+
+  if (close.length) {
+    for (let i = 0; i < close.length; i++) {
+      close[i].addEventListener("click", function () {
+        for (let j = 0; j < menu.length; j++) {
+          menu[j].classList.toggle("hidden");
+        }
+      });
+    }
+  }
+
+  if (backdrop.length) {
+    for (let i = 0; i < backdrop.length; i++) {
+      backdrop[i].addEventListener("click", function () {
+        for (let j = 0; j < menu.length; j++) {
+          menu[j].classList.toggle("hidden");
+        }
+      });
+    }
+  }
 });
